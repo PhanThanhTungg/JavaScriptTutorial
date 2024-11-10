f4b50212645e6baca105a8324865b2227a3643c7
TungPhan03 <tuttutxytin@gmail.com>
2024-11-10 17:59:26 +0700

    extend interface

---
+++ typeScriptTutorial/b1/js/ts.js
@@ -0,0 +1,5 @@
+let a = {
+    username: "tung",
+    cardId: "034032"
+};
+console.log(a);
--- typeScriptTutorial/b1/ts.ts
+++ typeScriptTutorial/b1/ts.ts
@@ -36,3 +36,20 @@
 // console.log(oba)
 
 //----------------extend interface---------------
+interface account{
+  username: string
+}
+
+interface user extends account{
+  cardId: string
+}
+
+interface admin extends account{
+  roleId: string
+}
+
+let a:user={
+  username: "tung",
+  cardId: "034032"
+}
+console.log(a)
