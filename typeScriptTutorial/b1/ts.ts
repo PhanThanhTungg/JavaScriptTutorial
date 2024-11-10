e4feadb1e91296c7327e0673904060ae1eb4a262
TungPhan03 <tuttutxytin@gmail.com>
2024-11-10 17:39:39 +0700

    update

---
+++ typeScriptTutorial/b1/ts.ts
@@ -0,0 +1,38 @@
+// let a: number = 10;
+// let b: string = "tung";
+// let c: boolean = true;
+// let ob:{
+//   name: string,
+//   age: number
+// } = {
+//   name: "tung",
+//   age: 18
+// }
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(ob)
+
+//----------------interface---------------
+// interface social{
+//   facebook?:string,
+//   zalo?:string
+// }
+
+// interface user{
+//   name: string,
+//   class: string,
+//   age?: number //optional
+//   social?: social
+// }
+
+// let oba:user ={
+//   name:"tung",
+//   class:"cn07",
+//   social:{
+//     facebook:"tung.com"
+//   }
+// }
+// console.log(oba)
+
+//----------------extend interface---------------
